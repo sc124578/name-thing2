c296020bf6ddfd40a864a83d9a023661bba9a775
Scott Knell <scottknell455@hotmail.com>
2023-03-17 03:49:50 -0600

    new

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,9 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
-      .then((res) => res.json())
-      .then((nme) => setName([nme]))
-  }, [])
+  fetch('https://starter-name-generator.onrender.com/api/generate') // Change this URL to the one that you just deployed
+    .then((res) => res.json())
+    .then((name) => setName([name]))
 
   return (
     <h1>{name}</h1>
